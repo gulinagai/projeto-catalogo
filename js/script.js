9613082ec8242a49199760b2204ccef146e287b5
Gustavo Nagai <148737051+gulinagai@users.noreply.github.com>
2024-02-13 10:37:21 -0300

    background-change adicionado

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 import NavTab from './modules/nav-tab.js'
 import SlideNav from './modules/slide.js'
 import Modal from './modules/modal.js'
+import AstaYunoMode from './modules/asta-yuno-mode.js'
 
 const navTab = new NavTab('.capitaes img', '.icones img')
 navTab.init()
+
+const astaYunoMode = new AstaYunoMode('.irmaos img', '.irmaos')
+astaYunoMode.init()
 
 const modal = new Modal('.quizz button', '.modal-container')
 modal.init()
